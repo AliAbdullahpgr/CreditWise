68dfea261286da630bfcb28d181bcff4b949d345
ali zu <ilabhaia1234@gmail.com>
2025-10-11 05:49:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "hisaabscore-12712437-b58e8",
+  "appId": "1:875882767287:web:dee64862d59c969f583e32",
+  "apiKey": "AIzaSyAuffttEqZI1ysHVSMv4s1F4LWCNmrsIw0",
+  "authDomain": "hisaabscore-12712437-b58e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "875882767287"
+};
